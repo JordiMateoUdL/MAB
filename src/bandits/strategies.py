575bea48af7e2bbd6a12889faa948c3945f5ff56
JordiMateoUdL <jordi.mateo@udl.cat>
2023-06-05 11:50:10 +0200

    (feat): improving docs

--- src/bandits/strategies.py
+++ src/bandits/strategies.py
@@ -1,22 +1,26 @@
+""" Strategy classes for selecting an arm in a multi-armed bandit problem. """
 from abc import ABC, abstractmethod
-from typing import Any, List, Union
+from typing import List, Union
 import numpy as np
+
 from domain.arm import Arm
 
+
 class Strategy(ABC):
-    
+
     """
-    Abstract base class representing a strategy for selecting an arm in a multi-armed bandit problem.
+    Abstract base class representing a strategy for selecting 
+    an arm in a multi-armed bandit problem.
     """
-    
-    def __init__(self, arms:List[Arm]):
-        self.arms:List[Arm] = arms
-        self.num_arms:int = len(arms)
-        self.best_reward:Union(int,float) = np.max([arm.true_reward for arm in arms])
-        self.exploration_steps:int = 0
-        self.exploitation_steps:int = 0
-        pass
-    
+
+    def __init__(self, arms: List[Arm]):
+        self.arms: List[Arm] = arms
+        self.num_arms: int = len(arms)
+        self.best_reward: Union(int, float) = np.max(
+            [arm.true_reward for arm in arms])
+        self.exploration_steps: int = 0
+        self.exploitation_steps: int = 0
+
     @abstractmethod
     def select_arm(self) -> int:
         """
@@ -25,27 +29,29 @@ class Strategy(ABC):
         Returns:
             The index of the selected arm.
         """
-        
+
         raise NotImplementedError("select_arm method must be implemented...")
-        
     
+
+
 class EpsilonGreedy(Strategy):
-    
+
     """
     Epsilon-Greedy strategy for selecting an arm
     """
-    
-    def __init__(self, arms:List[Arm], epsilon:float=0.1):
+
+    def __init__(self, arms: List[Arm], epsilon: float = 0.1):
         """
         Initializes the EpsilonGreedy strategy.
 
         Args:
             arms (List[Arm]): The list of arms available.
-            epsilon (float): The exploration rate (0 to 1) for balancing exploration and exploitation.
+            epsilon (float): The exploration rate (0 to 1) 
+            for balancing exploration and exploitation.
         """
         super().__init__(arms=arms)
-        self.epsilon:float = epsilon
-    
+        self.epsilon: float = epsilon
+
     def select_arm(self) -> int:
         """
         Selects an arm based on the Epsilon-Greedy strategy.
@@ -56,78 +62,118 @@ class EpsilonGreedy(Strategy):
         if np.random.random() < self.epsilon:
             self.exploration_steps += 1
             return np.random.randint(0, self.num_arms)
-        else:
-            self.exploitation_steps += 1
-            return np.argmax([arm.cumulative_reward for arm in self.arms])
-    
+
+        self.exploitation_steps += 1
+        return np.argmax([arm.cumulative_reward for arm in self.arms])
+
     def __str__(self) -> str:
         """
         Returns:
             A string representing the EpsilonGreedy strategy.
         """
         return f'EpsilonGreedy(e={self.epsilon})'
-    
+
 
 class UCB1(Strategy):
-    
-    def __init__(self, arms:List[Arm], c:Union[int,float]=1.0):
+    """UCB1 (Upper Confidence Bound 1) strategy for selecting an arm"""
+
+    def __init__(self, arms: List[Arm], c: Union[int, float] = 1.0, threshold: float = 1e-8):
         """
         Initializes the UCB1 strategy.
 
         Args:
             arms (List[Arm]): The list of arms available.
-            c (Union[int,float]): The exploration parameter
+            c (Union[int,float]): The exploration parameter.
+            threshold (float): A small value added to avoid division 
+            by zero when calculating exploration term.
         """
         super().__init__(arms=arms)
-        self.t:int = 0 
-        self.c:Union(int,float) = c
-        
-    def __str__(self) -> str:
-        return f'UCB1(c={self.c})'
-    
-    def select_arm(self) -> int:
-                
-        selected = np.argmax([arm.cumulative_reward + (self.c * np.sqrt(np.log(self.t) / (arm.pull_counts + 1e-8))) if arm.pull_counts > 0 else float('inf') for arm in self.arms])
+        self.t_value: int = 0
+        self.c_value: Union[int, float] = c
+        self.threshold: float = threshold
 
-        self.t += 1
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the UCB1 strategy.
+
+        Returns:
+            str: The string representation of the UCB1 strategy.
+        """
+        return f'UCB1(c={self.c_value})'
+
+    def select_arm(self) -> int:
+        """
+        Selects the arm based on the UCB1 strategy.
+
+        Returns:
+            int: The index of the selected arm.
+        """
+        selected = np.argmax(
+            [arm.cumulative_reward
+             + (self.c_value * np.sqrt(np.log(self.t_value) / (arm.pull_counts + self.threshold)))
+             if arm.pull_counts > 0 else float('inf') for arm in self.arms])
+
+        self.t_value += 1
         return selected
 
+
 class ThomsonSampling(Strategy):
-    
-    def __init__(self, arms:List[Arm], threshold:Union[int,float]=0.5):
+    """
+    Thompson Sampling strategy for the multi-armed bandit problem.
+    """
+
+    def __init__(self, arms: List[Arm], threshold: Union[int, float] = 0.5):
         """
-        Initializes the UCB1 strategy.
+        Initializes the Thomson Sampling strategy.
 
         Args:
             arms (List[Arm]): The list of arms available.
+            threshold (Union[int, float]): The threshold for considering a reward as a success.
         """
         super().__init__(arms=arms)
-        self.successes:List[int] = [1]*len(arms)
-        self.failures:List[int] = [1]*len(arms)
-        self.threshold:Union[int,float] = threshold
- 
-        
+        self.successes: List[int] = [1]*len(arms)
+        self.failures: List[int] = [1]*len(arms)
+        self.threshold: Union[int, float] = threshold
+
     def __str__(self) -> str:
+        """
+        Returns a string representation of the strategy.
+        """
         return f'ThomsonSampling (threshold={self.threshold})'
-    
-    def select_arm(self) -> int:      
-        seleceted = np.argmax([np.random.beta(self.successes[arm], self.failures[arm]) for arm in range(len(self.arms))])
-    
-        return seleceted
-    
-    def update(self, reward:float, arm:int):
+
+    def select_arm(self) -> int:
+        """
+        Selects an arm to play based on the Thompson Sampling strategy.
+
+        Returns:
+            int: The index of the selected arm.
+        """
+        selected = np.argmax([np.random.beta(
+            self.successes[arm], self.failures[arm]) for arm in range(len(self.arms))])
+
+        return selected
+
+    def update(self, reward: float, arm: int):
+        """
+        Updates the success and failure counts based on the received reward.
+
+        Args:
+            reward (float): The reward obtained from playing the selected arm.
+            arm (int): The index of the selected arm.
+        """
         if reward > self.threshold:
             self.successes[arm] += 1
         else:
             self.failures[arm] += 1
-    
+
+
 class PureExploration(Strategy):
-    
+
     """
     Pure exploration strategy for selecting an arm. Always selects a random arm.
     """
-    
-    def __init__(self,arms:List[Arm]):
+
+    def __init__(self, arms: List[Arm]):
         """
         Initializes the PureExploration strategy.
 
@@ -135,7 +181,7 @@ class PureExploration(Strategy):
             arms (List[Arm]): The list of arms available in the bandit problem.
         """
         super().__init__(arms=arms)
-        
+
     def select_arm(self):
         """
         Selects an arm based on the PureExploration strategy.
@@ -145,7 +191,7 @@ class PureExploration(Strategy):
         """
         self.exploration_steps += 1
         return np.random.randint(0, self.num_arms)
-    
+
     def __str__(self) -> str:
         """
         Returns:
@@ -153,12 +199,13 @@ class PureExploration(Strategy):
         """
         return "PureExploration"
 
+
 class PureExploitation(Strategy):
     """
     Pure exploitation strategy for selecting an arm. Always selects the arm with the highest cumulative reward.
     """
-    
-    def __init__(self,arms:List[Arm]):
+
+    def __init__(self, arms: List[Arm]):
         """
         Initializes the PureExploration strategy.
 
@@ -166,7 +213,7 @@ class PureExploitation(Strategy):
             arms (List[Arm]): The list of arms available.
         """
         super().__init__(arms=arms)
-        
+
     def select_arm(self):
         """
         Selects an arm based on the PureExploration strategy.
@@ -175,7 +222,7 @@ class PureExploitation(Strategy):
         """
         self.exploitation_steps += 1
         return np.argmax([arm.cumulative_reward for arm in self.arms])
-    
+
     def __str__(self) -> str:
         """
         Returns:
--- src/main.py
+++ src/main.py
@@ -8,36 +8,36 @@ import copy
 
 if __name__ == "__main__":
 
-    EXPLORATION_RATIO = 0.15    
-    slot_machines_config =[
+    EXPLORATION_RATIO = 0.15
+    slot_machines_config = [
         {"true_reward": 1.5},
         {"true_reward": 2.0},
         {"true_reward": 3.0}
     ]
-    
+
     slot_machines = []
     for config in slot_machines_config:
         slot_machines.append(SlotMachine(**config))
-        
+
     strategies = [
         PureExploitation(arms=copy.deepcopy(slot_machines)),
         PureExploration(arms=copy.deepcopy(slot_machines)),
-        EpsilonGreedy(arms=copy.deepcopy(slot_machines), epsilon=EXPLORATION_RATIO),
+        EpsilonGreedy(arms=copy.deepcopy(slot_machines),
+                      epsilon=EXPLORATION_RATIO),
         UCB1(arms=copy.deepcopy(slot_machines), c=1.5),
         ThomsonSampling(arms=copy.deepcopy(slot_machines), threshold=2.5)
     ]
-    
+
     plot_strategies(bandit_class=SlotMachineBandit,
-                   strategies=strategies,
-                   plot_type='regret',
-                   ylabel='Cumulative Regret',
-                   num_rounds=1000)
-    
+                    strategies=strategies,
+                    plot_type='regret',
+                    ylabel='Cumulative Regret',
+                    num_rounds=1000)
+
     # for strategy in strategies:
     #     bandit = SlotMachineBandit(strategy=strategy)
     #     best_arm = bandit.run(num_rounds=1000)
-    
+
     #     metrics = BanditMetrics(bandit)
     #     metrics.plot_cumulative_rewards()
     #     metrics.plot_histogram_arm_selections()
-    
